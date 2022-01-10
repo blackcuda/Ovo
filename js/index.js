0555e3724d52ecb7390aac0756a1d590d2a4233d
Sanne <sanne.krutzen@hotmail.com>
2022-01-10 22:10:19 +0100

    Hamburger menu geintegreerd

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
-var checkBox = document.getElementById("menyAvPaa");
-
-var navLinks = document.querySelectorAll(".navLink");
-
-for (var i = 0; i < navLinks.length; i++) {
-	var navLink = navLinks[i];
-	navLink.onclick = function() {
-		checkBox.checked = false;
-	}
+function toggleMobileMenu(menu)
+{
+	menu.classList.toggle('open');
 }
-
