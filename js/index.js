67901044b3254a12819de47c51927364f1367c26
Sanne <sanne.krutzen@hotmail.com>
2022-01-16 22:02:48 +0100

    sticky header, menu items en sizing pic2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+window.onscroll = function() {
+	handleStickyHeader()
+};
+
+var header = document.getElementById("pageHeader");
+var sticky = header.offsetTop;
+
+function handleStickyHeader()
+{
+	if (window.pageYOffset > sticky)
+	{
+		document.getElementById('pageHeader').classList.add("sticky");
+	}
+	else
+	{
+		document.getElementById('pageHeader').classList.remove("sticky");
+	}
+}
